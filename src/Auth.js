3359d9c77ef7e5cdd887a2816ccabbad5993527e
olaz <o.zaprzalka@gmail.com>
2021-04-09 22:12:05 +0200

    change auth

--- src/Auth.js
+++ src/Auth.js
@@ -1,17 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import app from "./base.js";
 
-export const AuthContext = React.createContext();
+export const AuthContext = React.createContext(
+  // currentUser: null,
+  // setCurrentUser: () => {}
+);
+
+// const stateSwitch =  () => {
+// const {currentUser, setCurrentUser} = useContext(AuthContext)
+// }
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  // let { currentUser } = useContext(AuthContext);
   // const [pending, setPending] = useState(true);
   useEffect(() => {
+    console.log('auth provider')
+    if (currentUser) {
+      return
+    }
+    console.log('auth provider 2')
+
     app.auth().onAuthStateChanged((user) => {
         if (user) {
             setCurrentUser(user)
             // setPending(false)
-            // console.log(pending)
+            console.log('pending')
             console.log('from auth', user.uid)
         } else {
             setCurrentUser(null)

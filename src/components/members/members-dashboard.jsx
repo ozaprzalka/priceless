d8e836ad1f253e5247e1304598331392e5c44ac9
grzegorz-kowalski-coderslab <49236120+grzegorz-kowalski-coderslab@users.noreply.github.com>
2021-04-09 17:59:18 +0200

    Update members-dashboard.jsx
    Dodałem przykład jak pobierać dane z Firebase oraz jak zapisywać. Radziłbym Ci jeszcze stworzyć kolekcję wewnątrz dokumentu user w Firebase, w tej chwili będziesz nadpisywała cały czas jedno pole w dokumencie. Dodatkowo trzeba napisać konwerter danych z Firebase na obiekty, których będziesz używać w wyświetlaniu listy linków. W dokumentacji Firebase jest wszystko ładnie opisane - https://firebase.google.com/docs/firestore/query-data/get-data

--- src/components/members/members-dashboard.jsx
+++ src/components/members/members-dashboard.jsx
@@ -1,86 +1,103 @@
-import React, {useContext, useState, useCallback} from 'react';
-import app from './../../base'
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import app, { db } from "./../../base";
 import { Redirect } from "react-router";
 import { AuthContext } from "../../Auth";
-import {database} from "../../base"
+import { database } from "../../base";
 import { formStyle } from "../../styles";
-import { withRouter } from "react-router";
 
 import {
-    Box,
-    Button,
-    Form,
-    FormField,
-    TextInput,
-    Paragraph,
-    List
-  } from "grommet";
+  Box,
+  Button,
+  Form,
+  FormField,
+  TextInput,
+  Paragraph,
+  List,
+} from "grommet";
 
-const MembersDashboard = ({history}) => {
-    const {currentUser} = useContext(AuthContext);
-    const [link, setLink] = useState();
-    const [links, setLinks] = useState([]);
+const MembersDashboard = ({ history }) => {
+  const { currentUser } = useContext(AuthContext);
+  const [link, setLink] = useState();
+  const [links, setLinks] = useState([]);
 
-    const handleLogout = useCallback (async e => {
-        e.preventDefault();
-        try {
-            app.auth().signOut();
-            (console.log('out'));
-            history.push("/login");
-          } catch (err){
-           console.log(err)
-          } 
-        return (
-            <Redirect to="/login" />
-        )
-    }, [history]);
+  const handleLogout = useCallback(
+    async (e) => {
+      e.preventDefault();
+      try {
+        app.auth().signOut();
+        console.log("out");
+        history.push("/login");
+      } catch (err) {
+        console.log(err);
+      }
+      return <Redirect to="/login" />;
+    },
+    [history]
+  );
 
-    const addLink = (e) => {
-        e.preventDefault();
-        console.log(link)
-        setLinks(links => [...links, link])
-        console.log(links);
-        let savedLink = {
-            links: [...links, link]
-        }
-        database.collection("links").doc(currentUser.uid).set(savedLink)
+  const addLink = (e) => {
+    if (link && link != undefined) {
+      database.collection("links").doc(currentUser.uid).set({
+        link: link
+      });
     }
-    const handleChange = (e) => {
-        e.preventDefault();
-        setLink(e.target.value)
+  };
+
+  const getLinks = () => {
+    if (currentUser && database.collection("links").doc(currentUser.uid)) {
+      let myLinks = database.collection("links").doc(currentUser.uid);
+      myLinks.get().then((doc) => {
+          if (doc.exists) {
+            console.log("Doc data: " + doc.data());
+          }
+          else {
+            console.log("Nie ma");
+          }
+        } 
+      )
+    } else {
+      return;
     }
+  };
 
-    // const renderLinks = (e) => {
-    //     database.collection("links").get().then(links => {
-    //         links.forEach(doc => {
-    //             return (
-    //                 <Text>{doc}</Text>
-    //             )
-    //         })
-    //     })
-    // }
+  useEffect(() => {
+    getLinks();
+    return () => links;
+  }, []);
 
-    return (
-        <>
-        <Box fill align="center" justify="center" style={formStyle}>
-            <Button  onClick={handleLogout} primary>Sign out</Button>
-            <Box width="medium">
-                <Paragraph>Your saved links</Paragraph>
-                <Form onSubmit={addLink}>
-                    <FormField value={link} onChange={handleChange}>
-                        <TextInput ></TextInput>
-                    </FormField>
-                    <Box>
-                    <Button type="submit" primary>Add new</Button>
-                    </Box>
-                </Form>
-                <List data={links}></List>
-                
-                {/* <Text>{currentUser.email}</Text> */}
+
+  return (
+    <>
+      <Box
+        fill
+        align="center"
+        justify="center"
+        style={formStyle}
+        onLoad={getLinks}
+      >
+        <Button onClick={handleLogout} primary>
+          Sign out
+        </Button>
+        <Box width="medium">
+          <Paragraph>Your saved links</Paragraph>
+          <Form onSubmit={addLink}>
+            <FormField>
+              <TextInput value={link} onChange={(e) => setLink(e.target.value)} />
+            </FormField>
+            <Box>
+              <Button type="submit" primary>
+                Add new
+              </Button>
             </Box>
+          </Form>
+          <Button type="submit" onClick={getLinks}>
+            Refresh
+          </Button>
+          <List data={links}></List>
         </Box>
-        </>
-    )
-}
+      </Box>
+    </>
+  );
+};
 
-export default withRouter(MembersDashboard);
+export default MembersDashboard;

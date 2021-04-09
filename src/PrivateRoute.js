10d13982ec2aa79b006e4d7c44cc358728937ffb
grzegorz-kowalski-coderslab <49236120+grzegorz-kowalski-coderslab@users.noreply.github.com>
2021-04-09 17:52:14 +0200

    Update PrivateRoute.js
    Tutaj przykład jak wykonać PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,44 +1,16 @@
-import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import { AuthContext } from "./Auth";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
-  const { currentUser } = useContext(AuthContext);
-  return (
-      <Route {...rest} render={props => (
-          currentUser ?
-              <Component {...props} />
-          : <Redirect to="/login" />
-      )} />
-  );
+    const { currentUser } = useContext(AuthContext);
+    return (
+        <Route {...rest} render={props => (
+            currentUser ?
+                <Component {...props} />
+            : <Redirect to="/login" />
+        )} />
+    );
 };
-
-// const PrivateRoute3 = ({ comp: Component, ...rest }) => {
-//   const { currentUser } = useContext(AuthContext);
-//   return (
-//     <Route
-//       {...rest}
-//       render={(props) =>
-//         !currentUser ? (
-//           <Redirect to="/login" />
-//         ) : (
-//           <Component {...props}></Component>
-//         )
-//       }
-//     ></Route>
-//   );
-// };
-
-// const PrivateRoute2 = ({
-//   comp: Component, // use comp prop
-//   ...rest
-// }) => (
-//   <Route
-//     {...rest}
-//     render={(props) =>
-//       !currentUser ? <Redirect to="/login" /> : <Component {...props} />
-//     }
-//   />
-// );
 
 export default PrivateRoute;
